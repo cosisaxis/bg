bd40868edd048c3021c2aa9c81ab242024addcd7
cosisaxis <kishanelewis2@gmail.com>
2022-06-23 18:24:28 -0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Productlist from "./pages/Productlist";
 import SignUp from "./pages/SignUp";
 
 const App = () => {
-  return < SignUp />;
+  return < Productlist />;
 };
 
 export default App;
