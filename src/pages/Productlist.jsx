ed38ccd33b1ac43d8c4c4ec8347191fef4106c61
cosisaxis <kishanelewis2@gmail.com>
2022-06-23 18:31:09 -0500

    product-list responsive

--- src/pages/Productlist.jsx
+++ src/pages/Productlist.jsx
@@ -4,7 +4,7 @@ import Deals from "../components/Deals";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
-
+import { mobile } from "../responsiviness";
 
 const Container = styled.div``;
 
@@ -19,6 +19,8 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({width: "0px 20px", display:"flex", flexDirection: "column"})}
+
  
 `;
 
@@ -26,12 +28,14 @@ const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({marginRight: "0"})}
 
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
   
 `;
 const Option = styled.option``;
